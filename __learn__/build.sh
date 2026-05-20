f3fb0a6a4a79ec7b6a90d5c29874ad81443f7288
XodiumLuma <28709874+xodiumluma@users.noreply.github.com>
2026-05-20 18:26:32 +1000

    [learn] finish build script *nix

--- __learn__/build.sh
+++ __learn__/build.sh
@@ -7,3 +7,10 @@ if [ "$OS" = "Darwin" ]; then
 else
 	DIR_OS="Linux"
 fi
+
+if [[ "$*" == *"--ios"* ]]; then
+	DIR_OS="iOS"
+elif [[ "$*" == *"--android"* ]]; then
+	DIR_OS="Android"
+fi
+python3 "$DIR"/tools/ci_build/build.py --build_dir "$DIR"/build/$DIR_OS "$@"
