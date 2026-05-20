6f2c1e71f2a6ff3bee5ecf0f84d59d3c0dd38839
XodiumLuma <28709874+xodiumluma@users.noreply.github.com>
2026-05-20 18:13:18 +1000

    [learn] start build script *nix

--- __learn__/build.sh
+++ __learn__/build.sh
@@ -2,3 +2,8 @@
 # Find out the absolute path of this directory
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 OS=$(uname -s)
+if [ "$OS" = "Darwin" ]; then
+	DIR_OS="MacOS"
+else
+	DIR_OS="Linux"
+fi
